bc2ecef4d8dcffe9c771e50e715f06c462288f13
jos <jos.kafer@univ-lyon1.fr>
2022-05-03 11:37:55 +0200

    Added script using V.PhyloMaker to extract a phylogeny for our species from a megaphylogeny

---
+++ rscripts/prune_megatree.R
@@ -0,0 +1,11 @@
+library(ape)
+library(V.PhyloMaker)
+library(Taxonstand)
+
+qry_div=read.csv("qryDiveRS_Data_2022-04-29.csv")
+spec_list=as.array(row.names(table(qry_div$NTaxDat)))
+tpl_list= TPL(spec_list)
+spec_list=tpl_list[,c("Taxon","New.Genus","Family")]
+maker_tree=phylo.maker(spec_list)
+plot(maker_tree$scenario.3)
+write.tree(maker_tree$scenario.3,file="pruned_tree.tre")
