dd209bb976a3ca251d9d76b0628b5e8c75bba577
sylvainglemin <62335084+sylvainglemin@users.noreply.github.com>
2023-08-30 12:35:26 +0200

    Adding two scripts: 1: morphospace_SG.r to replot all traits on the pcoa spaces + non-linear dimensional scaling plots, 2: simul_phylo_SG.r ro simul the traits independently on the phylogeny and analyse the simulated morphospaces. Corresponding figures and outputs are commited too.

---
+++ rscripts/morphospace_SG.R
@@ -0,0 +1,216 @@
+library(ape)
+library(cluster)
+library(umap)
+library(ggplot2)
+library(ggpubr)
+
+# This script re-generate initial pcoa plots from gower's distance
+# Then it compare them to the same analyses with imputed data and recoding in multistate characters used for phylogenetic analysis
+# In addition, the results are compared with non-linear dimensional scaling
+
+# Overall, the differences are rather limited, at least for the main patterns
+# The imputed and recoded dataset seems to amplify the differences and the clustering
+
+# Loading and formating datasets
+df_final <- readRDS("outputs/6_df_filt_trans.rds")
+df_onehot <- readRDS("outputs/one_hot_6_df_filt_trans.rds")
+df_imputed <- read.csv("outputs/one_hot_imputed_with_phylo.csv")
+
+# Re-ordering some levels for plotting
+df_final$DispersalMode <- factor(
+  df_final$DispersalMode,
+  levels = c("autonomous",
+             "abiotic_autonomous",
+             "abiotic",
+             "autonomous_biotic",
+             "abiotic_autonomous_biotic",
+             "abiotic_biotic",
+             "biotic")
+  )
+df_final$Pollination <- factor(
+  df_final$Pollination,
+  levels = c("autonomous",
+             "abiotic_autonomous",
+             "abiotic",
+             "autonomous_biotic",
+             "abiotic_biotic",
+             "biotic")
+)
+df_final$Mating <- factor(
+  df_final$Mating,
+  levels = c("selfing",
+             "mixed",
+             "outcrossing")
+)
+
+# Reformating the imputed dataset
+# The initial dataset uses the one-hot encoding
+# For discrete traits with more than two states this is not appropriate for evolutionary model fitting
+# In addition for "paired traits" due to one-hot encoding, only one need to be simulated
+# As the re-coding depend on the traits, this is done manually
+# Note also that for traits with more than two states they can be ordered and some transitions can be set to 0 (see below)
+
+# Split the initial matrix into continuous and discrete trait matrices
+species_names <- df_imputed$Species
+continous_traits <- df_imputed[,c(2:8)]
+discrete_traits <- df_imputed[,c(9:43)]
+
+# List of states per trait
+lapply(df_final,levels)
+
+# To transform onehot encoded variable into multistate variable
+# state 1 = 0, state 2 = 2, polymorphic state = 1
+onehot2multi <- function(w,x,y=0,z=0) {
+  return(2*x - w*x +5*y -2*w*y - 3*x*y + 9*z - 3*w*z -4*x*z -6*y*z)
+}
+
+# Two-state traits
+woodiness <- as.factor(onehot2multi(df_imputed$Woodiness_woody,df_imputed$Woodiness_herbaceous))
+climbing <- as.factor(onehot2multi(df_imputed$Climbing_non.climbing,df_imputed$Climbing_climbing))
+aquatic <- as.factor(onehot2multi(df_imputed$Aquatic_non.aquatic,df_imputed$Aquatic_non.aquatic))
+sexualsystem <- as.factor(onehot2multi(df_imputed$SexualSystem_monomorphic,df_imputed$SexualSystem_dimorphic))
+lifespan <- as.factor(onehot2multi(df_imputed$Lifespan_long,df_imputed$Lifespan_short))
+dispersaldist <- as.factor(onehot2multi(df_imputed$DispersalDist_long,df_imputed$DispersalDist_short))
+flowersex <- as.factor(onehot2multi(df_imputed$FlowerSex_bisexual,df_imputed$FlowerSex_unisexual))
+showiness <- as.factor(onehot2multi(df_imputed$Showiness_bright,df_imputed$Showiness_dull))
+# Three-state traits
+pollination <- as.factor(onehot2multi(df_imputed$Pollination_autonomous,df_imputed$Pollination_abiotic,df_imputed$Pollination_biotic))
+dispersalmode <- as.factor(onehot2multi(df_imputed$DispersalMode_autonomous,df_imputed$DispersalMode_abiotic,df_imputed$DispersalMode_biotic))
+ovaryposition <- as.factor(onehot2multi(df_imputed$OvaryPosition_superior,df_imputed$OvaryPosition_intermediate,df_imputed$OvaryPosition_inferior))
+flowersymmetry <- as.factor(onehot2multi(df_imputed$FlowerSymmetry_actinomorphic,df_imputed$FlowerSymmetry_zygomorphic,df_imputed$FlowerSymmetry_other))
+floralreward <- as.factor(onehot2multi(df_imputed$FloralReward_none,df_imputed$FloralReward_pollen,df_imputed$FloralReward_nectar,df_imputed$FloralReward_other))
+# Special case - polymorphic cases are considered as mixed
+matingsystem <- as.factor(onehot2multi(df_imputed$Mating_selfing,df_imputed$Mating_mixed,df_imputed$Mating_outcrossing))
+matingsystem <- as.factor(ifelse(matingsystem=="3" | matingsystem=="4",2,matingsystem))
+
+discrete_traits_recoded <- data.frame(
+  list(
+    "woodiness"=woodiness,
+    "climbing"=climbing,
+    "aquatic"=aquatic,
+    "dioecy"=sexualsystem,
+    "flowersex"=flowersex,
+    "lifespan"=lifespan,
+    "showiness"=showiness,
+    "dispersaldist"=dispersaldist,
+    "dispersalmode"=dispersalmode,
+    "matingsystem"=matingsystem,
+    "pollination"=pollination,
+    "ovaryposition"=ovaryposition,
+    "flowersymetry"=flowersymmetry,
+    "reward"=floralreward
+  )
+)
+
+# Discrete traits as factor for the gower distance analysis
+df_recoded <- cbind(
+  continous_traits,
+  discrete_traits_recoded
+)
+saveRDS(df_recoded,"outputs/imputed_recoded.RDS")
+
+# Lists of  names
+traits_list_final <- names(df_final)
+traits_list_onehot <- names(df_onehot)
+traits_list_recoded <- names(df_recoded)
+
+# Computing distance matrices ####
+dist_final <- daisy(df_final,"gower")
+dist_onehot <- daisy(df_onehot,"gower")
+dist_recoded <- daisy(df_recoded,"gower")
+
+# Compare the different distances
+pdf("figures/pcoa/distance_comparison.pdf")
+pairs(
+  data.frame(list(
+    "final"=as.vector(dist_final),
+    "onehot"=as.vector(dist_onehot),
+    "recoded"=as.vector(dist_recoded))
+  ),pch=16,cex=0.5
+)
+dev.off()
+
+# Run pcoa and keep the 4 first axes
+pcoa_final <- data.frame(pcoa(dist_final)$vector[,c(1:4)])
+pcoa_onehot <- data.frame(pcoa(dist_onehot)$vector[,c(1:4)])
+pcoa_recoded <- data.frame(pcoa(dist_recoded)$vector[,c(1:4)])
+
+
+# Mapping the traits on the pcoa ####
+
+# Function to generate plot for all trait using
+plot_figure_pcoa <- function(df,tr,suffix,label_list) {
+  names(df) <- c("dim1","dim2","dim3","dim4")
+  label <- label_list[,tr]
+  if(is.numeric(label)) {
+      # color <- scale_colour_gradientn(colours=terrain.colors(10),name=NULL)
+      color <- scale_colour_viridis_c()
+  } else
+    color <- scale_color_discrete()
+  g12 <- ggplot(data=df,aes(x=dim1,y=dim2,col=label)) + geom_point() + color
+  g13 <- ggplot(data=df,aes(x=dim1,y=dim3,col=label)) + geom_point() + color
+  g14 <- ggplot(data=df,aes(x=dim1,y=dim4,col=label)) + geom_point() + color
+  g23 <- ggplot(data=df,aes(x=dim2,y=dim3,col=label)) + geom_point() + color
+  g24 <- ggplot(data=df,aes(x=dim2,y=dim4,col=label)) + geom_point() + color
+  g34 <- ggplot(data=df,aes(x=dim3,y=dim4,col=label)) + geom_point() + color
+  fig <- ggarrange(
+    plotlist = list(g12,g13,g14,g23,g24,g34),
+    ncol = 2,
+    nrow = 3,
+    common.legend = T
+  )
+  annotate_figure(fig,top = text_grob(tr, face = "bold", size = 14))
+  ggsave(paste0("figures/pcoa/",tr,"_pcoa_",suffix,".pdf"),width = 7,height = 10)
+}
+
+for(tr in traits_list_final) {
+  plot_figure_pcoa(df = pcoa_final,tr = tr,suffix = "final",label_list = df_final)
+  plot_figure_pcoa(df = pcoa_onehot,tr = tr,suffix = "onehot",label_list = df_final)
+  plot_figure_pcoa(df = pcoa_recoded,tr = tr,suffix = "recoded",label_list = df_final)
+}
+
+
+
+# Analysis with umap
+
+# The umap method can be applied either on the orginal dataset or on the distance matrix
+# We applied it on the distance matrix as it doesn't handle missing data
+
+plot_figure_umap<- function(df,tr,suffix,label_list) {
+  names(df) <- c("dim1","dim2")
+  label <- label_list[,tr]
+  if(is.numeric(label)) {
+    #color <- scale_colour_gradientn(colours=terrain.colors(10),name=NULL)
+    color <- scale_colour_viridis_c()
+  } else
+    color <- scale_color_discrete()
+  ggplot(data=df,aes(x=dim1,y=dim2,col=label)) + geom_point() + color
+  ggsave(paste0("figures/umap/",tr,"_umap_knn",knn,"_",suffix,".pdf"),width = 7,height = 7)
+}
+
+# The number of neighbors play a key role in the importance of the small scale versus the large scale
+# A their too many plots only the final distance matrix is used
+# But onehot and recoded matrices can be also run by encommenting the following code
+for(knn in c(5,10,50,100)) {
+  custom_config <- umap.defaults
+  custom_config$n_components <-  2# number of dimensions targeted
+  custom_config$n_neighbors <- knn # number of dimensions targeted
+  custom_config$input <- "dist" # The input matrix is a distance matrix
+  umap_final <- umap(d = as.matrix(dist_final),config = custom_config)
+  df_umap_final <- data.frame(umap_final$layout)
+  rownames(df_umap_final) <- species_names
+  #umap_onehot <- umap(d = as.matrix(dist_onehot),config = custom_config)
+  #df_umap_onehot <- data.frame(umap_onehot$layout)
+  #rownames(df_umap_onehot) <- species_names
+  #umap_recoded <- umap(d = as.matrix(dist_recoded),config = custom_config)
+  #df_umap_recoded <- data.frame(umap_recoded$layout)
+  #rownames(df_umap_recoded) <- species_names
+  for(tr in traits_list_final) {
+    plot_figure_umap(df = df_umap_final,tr = tr,suffix = "umap_final",label_list = df_final)
+    #plot_figure_umap(df = df_umap_onehot,tr = tr,suffix = "umap_onehot",label_list = df_final)
+    #plot_figure_umap(df = df_umap_recoded,tr = tr,suffix = "umap_recoded",label_list = df_final)
+  }
+}
+
+
+
---
+++ rscripts/simul_phylo_SG.R
@@ -0,0 +1,241 @@
+library(ape)
+library(phytools)
+library(geiger)
+library(castor)
+library(cluster)
+library(ggplot2)
+library(ggpubr)
+library(dplyr)
+
+
+
+# Loading the tree and the trait matrix ####
+angio_tree <- read.tree("outputs/pruned_tree.tre")
+df_final <- readRDS("outputs/6_df_filt_trans.rds")
+df_onehot <- readRDS("outputs/one_hot_6_df_filt_trans.rds")
+df_recoded <- readRDS("outputs/imputed_recoded.RDS")
+
+# Split the initial matrix into continuous and discrete trait matrices
+species_names <- angio_tree$tip.label
+continous_traits <- df_recoded[,c(1:7)]
+discrete_traits <- df_recoded[,c(8:21)]
+
+
+
+# Fitting evolution models for each trait ####
+
+# Function to fit an OU model to a continuous trait and return the model parameters
+fit_cont <- function(ttf) {
+  # rates
+  trait_to_fit <- ttf
+  names(trait_to_fit) <- species_names
+  fit <- fitContinuous(
+    phy = angio_tree,
+    dat = trait_to_fit,
+    model = "OU",
+    bounds = list(alpha=c(exp(-500),exp(5))) # max extended a bit / default
+    )
+  # ancestral state under the BM model
+  # OU should be used but it's very unstable
+  anc <- reconstruct(ttf,angio_tree,method = "ML")
+  mean_root <- anc$ace[1] # The first node is the root
+  sd_root <- sqrt((anc$CI95[1,2]-anc$CI95[1,1])/(2*1.96))
+  param <- c(unlist(fit$opt[c(1:3)]),mean_root,sd_root)
+  names(param) <- c("alpha","sigma","z0","mean_root","sd_root")
+  return(param)
+}
+# creating and saving the list of OU parameters
+ou_list <- lapply(continous_traits,fit_cont)
+saveRDS(ou_list,"outputs/ou_list.rds")
+
+# Function to transform the list of rates into the corresponding transition matrix
+# For the fitDiscrete function, not useful for the asr_mk_model function
+# rate2mat <- function(r) {
+#   k <- length(r)
+#   n <- as.integer((sqrt(4*k+1) + 1)/2)
+#   for(i in c(0:(n-1))) r <- append(r,0,i*n+i)
+#   mat <- matrix(r,n,n)
+#   for(i in c(1:n)) mat[i,i] <- -colSums(mat)[i]
+#   return(mat)
+# }
+
+# Function to fit a Mk model to a discrete trait and return the transition matrix
+fit_disc <- function(ttf) {
+  trait_to_fit <- as.numeric(ttf)
+  names(trait_to_fit) <- species_names
+  # Much more rapid than the fitDiscrete function
+  fit <- asr_mk_model(angio_tree,as.numeric(ttf),rate_model = "ARD")
+  #fit <- fitDiscrete(angio_tree,trait_to_fit,model = "ARD")
+  #rates <- unlist(fit$opt[1:fit$opt$k])
+  #mat <- rate2mat(rates)
+  print("done")
+  return(list("mat"=fit$transition_matrix,"anc_st"=fit$ancestral_likelihoods[1,]))
+}
+# creating and saving the list of transition matrices (take some time)
+mk_list <- lapply(discrete_traits,fit_disc)
+saveRDS(mk_list,"outputs/mk_list.rds")
+
+
+# Simulating independent traits according to the fitted evolution model ####
+
+# Function to simulate a dataset according to the evolutionary processes
+# Take as in put:
+# - A list of vectors with OU parameters + anc state for continuous characters
+# - A list of matrices with transition rates + anc state proba for discrete characters
+# - A tree
+# Return a data.frame with simulated values
+
+simul_dataset <- function(ou,mk,tree) {
+  # continuous traits
+  sim_continuous <- lapply(
+    ou,
+    function(x) rTraitCont(
+      phy = tree,
+      model = "OU",
+      alpha = x[1],
+      sigma = x[2],
+      theta =  x[3],
+      root.value = rnorm(1,x[4],x[5]))
+  )
+  # discrete traits
+  sim_discrete <- lapply(
+    mk,
+    function(x) {
+      st <- as.character(1:length(x$anc_st))
+      root <- sample(st,1,prob = x$anc_st)
+      as.factor(
+        sim.history(
+          tree = tree,
+          Q = x$mat,
+          anc = root,
+          nsim = 1,
+          message=F
+        )$states
+      )
+    }
+  )
+  df <- data.frame(sim_continuous,sim_discrete)
+  return(df)
+}
+
+
+#  Simulate n datasets ####
+# Can be uncommented to resimulate
+#ou_list <- readRDS("outputs/ou_list.rds")
+#mk_list <- readRDS("outputs/mk_list.rds")
+#n_sim <- 1000
+#set.seed(123)
+#simdata <- replicate(n_sim,simul_dataset(ou_list,mk_list,angio_tree),simplify = F)
+#saveRDS(simdata,"outputs/phylo_simulated_datasets.rds")
+
+
+# Analyzing simulations ####
+
+sim_list <- readRDS("outputs/phylo_simulated_datasets.rds")
+
+# Function to compute gower distance, run pcoa and return eigenvalues from one simulated data_frame
+pc_ev <- function(df) {
+  # We only keep the number of axes equal to the number of variables
+  n_var <- dim(df)[2]
+  # To ensure that discrete variables are factors
+  df[,c(8:21)] <- lapply(df[,c(8:21)],as.factor)
+  dist <- daisy(df,"gower")
+  pc <- pcoa(dist)
+  r_eig <- pc$values$Relative_eig
+  r_eig <- r_eig[r_eig>0]/sum(r_eig[r_eig>0])
+  return(r_eig[c(1:n_var)])
+}
+
+# Can be uncommented to recompute eigenvalues
+# sim_ev <- sapply(sim_list,pc_ev)
+# saveRDS(data.frame(t(sim_ev)),"outputs/phylo_simulated_ev.rds")
+
+
+# Analysing the results
+
+# Creating a dataset with observed an simulated ev
+sim_ev <- readRDS("outputs/phylo_simulated_ev.rds")
+sim_ev_mean <- apply(sim_ev,2,mean)
+sim_ev_min <- apply(sim_ev,2,function(x) quantile(x,probs = c(0.05)))
+sim_ev_max <- apply(sim_ev,2,function(x) quantile(x,probs = c(0.95)))
+ef <- pcoa(daisy(df_final,"gower"))$values$Relative_eig
+ef <- ef[ef>0]/sum(ef[ef>0])
+final_ev <- ef[c(1:21)]
+eo <- pcoa(daisy(df_onehot,"gower"))$values$Relative_eig
+eo <- eo[eo>0]/sum(eo[eo>0])
+onehot_ev <- eo[c(1:21)]
+er <- pcoa(daisy(df_recoded,"gower"))$values$Relative_eig
+er <- er[er>0]/sum(er[er>0])
+recoded_ev <- er[c(1:21)]
+rm(ef,eo,er)
+df_ev <- data.frame(
+  list(
+    "sim_mean"=sim_ev_mean,
+    "sim_min"=sim_ev_min,
+    "sim_max"=sim_ev_max,
+    "final"=final_ev,
+    "onehot"=onehot_ev,
+    "recoded"=recoded_ev)
+)
+# Plot
+ggplot(data = df_ev,aes(x=c(1:21))) +
+  geom_line(aes(y=final,col="final")) +
+  geom_point(aes(y=final,col="final")) +
+  geom_line(aes(y=onehot,col="onehot")) +
+  geom_point(aes(y=onehot,col="onehot")) +
+  geom_line(aes(y=recoded,col="recoded")) +
+  geom_point(aes(y=recoded,col="recoded")) +
+  geom_errorbar(aes(ymin=sim_min,ymax=sim_max)) +
+  xlab("Rank of eigenvalues") + ylab("Relative eigenvalues") +
+  labs(col = "Trait matrix")
+ggsave("figures/observed_vs_simulated_eigenvalues.pdf")
+
+
+# Looking at the traits in the simulated datasets
+# As traits are independent, each axis should +/- to one trait
+# In most simulations woodiness appears on Axis.1
+# On other axis it's often the same other traits: 3 examples  are given
+# TO INVESTIGATE MORE: it should be linked to the rate of evolution of the traits
+
+df_sim <- sim_list[[1]] # pick one dataset
+df_sim[,c(8:21)] <- lapply(df_sim[,c(8:21)],as.factor)
+pp <- data.frame(pcoa(daisy(df_sim,"gower"))$vectors)
+# Coloring the pcoa with different label
+g1 <- ggplot(data=pp,aes(x=Axis.1,y=Axis.2,col=df_sim$woodiness)) + geom_point()
+g2 <- ggplot(data=pp,aes(x=Axis.2,y=Axis.3,col=df_sim$dispersaldist)) + geom_point()
+ggarrange(plotlist = list(g1,g2),ncol = 1,nrow = 2)
+ggsave("figures/pcoa_sim_ex1.pdf")
+
+df_sim <- sim_list[[2]] # pick one dataset
+df_sim[,c(8:21)] <- lapply(df_sim[,c(8:21)],as.factor)
+pp <- data.frame(pcoa(daisy(df_sim,"gower"))$vectors)
+# Coloring the pcoa with different label
+g1 <- ggplot(data=pp,aes(x=Axis.1,y=Axis.2,col=df_sim$woodiness)) + geom_point()
+g2 <- ggplot(data=pp,aes(x=Axis.2,y=Axis.3,col=df_sim$showiness)) + geom_point()
+ggarrange(plotlist = list(g1,g2),ncol = 1,nrow = 2)
+ggsave("figures/pcoa_sim_ex2.pdf")
+
+df_sim <- sim_list[[3]] # pick one dataset
+df_sim[,c(8:21)] <- lapply(df_sim[,c(8:21)],as.factor)
+pp <- data.frame(pcoa(daisy(df_sim,"gower"))$vectors)
+# Coloring the pcoa with different label
+g1 <- ggplot(data=pp,aes(x=Axis.1,y=Axis.2,col=df_sim$woodiness)) + geom_point()
+g2 <- ggplot(data=pp,aes(x=Axis.2,y=Axis.3,col=df_sim$flowersex)) + geom_point()
+ggarrange(plotlist = list(g1,g2),ncol = 1,nrow = 2)
+ggsave("figures/pcoa_sim_ex3.pdf")
+
+
+
+# df_rates <- data.frame(
+#   trait = as.factor(names(mk_list)),
+#   #rate = as.numeric(unlist(lapply(mk_list,function(x) mean(abs(x$mat)))))
+#   rate = as.numeric(unlist(lapply(mk_list,function(x) max(abs(x$mat)))))
+#   )
+# 
+# df_rates %>%
+#   arrange(desc(rate)) %>%
+#   mutate(trait=factor(trait, levels=trait)) %>%
+#   ggplot(aes(x=trait, y=rate,fill=trait)) + 
+#     geom_bar(stat = "identity",show.legend = F) +
+#     theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+#     xlab("Traits") + ylab("Evolutionary rates")
