1da84c58dc92fb2b23dd7bf997cc497a46c171b7
Andrew Helmstetter <andrew.j.helmstetter@gmail.com>
2024-05-13 12:07:57 +0200

    added correlation to trait selection

--- rscripts/utility/trait_selection.R
+++ rscripts/utility/trait_selection.R
@@ -19,13 +19,15 @@ gower_df2 <- daisy(df2,
 #make into distance object
 dataset_dist2 <- stats::as.dist(gower_df2)
 
-
 #vector for order of columns dropped
 cols_dropped<-matrix(nrow=length(colnames(df))-1,ncol=2)
 
+####
+# ---- AUC ----
+####
+
 #empty list
 list_auc_changes<-list()
-
 
 for (i in 1:(length(colnames(df))-1)) {
   
@@ -35,6 +37,13 @@ for (i in 1:(length(colnames(df))-1)) {
     df3 <- df2[, -j]
     gower_df3 <- daisy(as.data.frame(df3), metric = "gower")
     dataset_dist3 <- stats::as.dist(gower_df3)
+    
+    
+    #This method was used in Dimensionality (Mouillot et al. 2021)
+    #Supposed to be used for measuring the effect of dimensionality reduction
+    #e.g. distance matrix vs pcoa
+    #but here comparing distance matrices with variables dropped
+    #unsure whether this is entirely appropriate
     Co_rank   <-
       coRanking::coranking(dataset_dist2, dataset_dist3, input_Xi = "dist")
     NX        <- coRanking::R_NX(Co_rank)
@@ -85,4 +94,144 @@ cols_dropped %>%
 #1: In rankmatrix(Xi, input = input_Xi, use = use) :
 #  0 outside of diagonal in distance matrix
 
+####
+# ---- Correlation ----
+####
+
+#Has problems when only a few traits with NAs in distance matrices
+
+#new df for editing
+df2<-df
+
+#dissimilarity matrix calculation of matrix
+gower_df2 <- daisy(df2,
+                   metric = "gower")
+
+#make into distance object
+dataset_dist2 <- stats::as.dist(gower_df2)
+
+#vector for order of columns dropped
+cols_dropped<-matrix(nrow=length(colnames(df))-1,ncol=2)
+
+list_cors<-list()
+
+for (i in 1:(length(colnames(df))-1)) {
+  
+  cors <- vector()
+  
+  for (j in 1:length(colnames(df2))) {
+    df3 <- df2[, -j]
+    gower_df3 <- daisy(as.data.frame(df3), metric = "gower")
+    dataset_dist3 <- stats::as.dist(gower_df3)
+    
+    #same as cor if not using p val
+    #cors[j]<-vegan::mantel(dataset_dist2,dataset_dist3)
+    
+    cors[j]<-cor(dataset_dist2,dataset_dist3)[1]
+    
+    }
+  
+  names(cors)<-colnames(df2)
+  
+  list_cors[[i]]<-cors
+  
+  #remove auc value with least change
+  df2 <- df2[, !colnames(df2) %in% names(sort(cors,decreasing=TRUE)[1])]
+  
+  #store name of column dropped
+  cols_dropped[i,1]<-names(sort(cors,decreasing=TRUE)[1])
+  cols_dropped[i,2]<-sort(cors,decreasing=TRUE)[1]
+  
+}
+
+cols_dropped<-na.omit(as.data.frame(cols_dropped))
+colnames(cols_dropped)<-c('trait','Cor')
+cols_dropped
+
+#add row for last trait remaining
+remaining_traits<-setdiff(colnames(df),cols_dropped$trait)
+
+remaining_traits_table<-cbind(remaining_traits,rep(0,length(remaining_traits)))
+colnames(remaining_traits_table)<-c('trait','Cor')
+
+cols_dropped<-rbind(cols_dropped,remaining_traits_table)
+
+cols_dropped$Cor<-as.numeric(cols_dropped$Cor)
+
+cols_dropped %>%
+  mutate(trait = fct_reorder(trait, rev(desc(Cor)))) %>%
+  ggplot( aes(x=trait, y=Cor)) +
+  geom_bar(stat="identity", fill="#f68060", alpha=.6, width=.4) +
+  coord_flip() +
+  xlab("") +
+  ylab("Correlation when removed to original distance matrix") +
+  theme_bw()
+
+####
+# ---- Redo PCoA ----
+####
+
+#load data set
+df<-readRDS(file = here::here("outputs/6_df_filt_trans.rds"))
+
+#dissimilarity matrix calculation
+gower_df <- daisy(df,
+                  metric = "gower" )
+summary(gower_df)
+
+#make into distance object
+dataset_dist <- stats::as.dist(gower_df)
+
+#run PCoA on distance matrix
+dataset_pcoa <- ape::pcoa(dataset_dist)
+
+#plot PCOA points on first two axes
+ggplot(data.frame(dataset_pcoa$vectors), aes(x = Axis.1, y = Axis.2)) +
+  geom_point(
+    color=1,
+    shape=16,
+    alpha=0.75,
+    size=2,
+    stroke = 0.5
+  )
+
+df2 <- subset(df, select = -c(seedMass,
+                              Numberofovulesperfunctionalcarpel,
+                              flowerSize,
+                              Numberofstructuralcarpels,
+                              Maximumverticalheight,
+                              Numberoffertilestamens,
+                              Fusionofovaries#,
+                              #Woodiness,
+                              #FlowerSex
+))
+
+# df2 <- subset(df, select = c(Woodiness,
+#                              Pollination,
+#                              FlowerSex,
+#                              DispersalMode,
+#                              Lifespan,
+#                              Showiness,
+#                              seedMass))
+
+#dissimilarity matrix calculation
+gower_df2 <- daisy(df2,
+                  metric = "gower" )
+summary(gower_df2)
+
+#make into distance object
+dataset_dist2 <- stats::as.dist(gower_df2)
+
+#run PCoA on distance matrix
+dataset_pcoa2 <- ape::pcoa(dataset_dist2)
+
+#plot PCOA points on first two axes
+ggplot(data.frame(dataset_pcoa2$vectors), aes(x = Axis.1, y = Axis.2)) +
+  geom_point(
+    color=1,
+    shape=16,
+    alpha=0.75,
+    size=2,
+    stroke = 0.5
+  )
 
